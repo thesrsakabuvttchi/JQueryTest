50cbdcaffa661b06eb7095a46b022d56be6fba73
pc <pc@pc-pc>
2021-05-20 17:05:42 +0530

    .

--- App.py
+++ App.py
@@ -1,4 +1,6 @@
+import re
 from flask import Flask,request,render_template
+import json
 
 class Employee:
     def __init__(self,Eid,Ename,Esal,HRA,DA,Ded):
@@ -21,11 +23,64 @@ class Employee:
 
 volatile_db = []
 
-app = Flask(__name__,static_url_path='', static_folder='./')
+app = Flask(__name__,static_url_path='', static_folder='',template_folder='')
 
 @app.route("/")
 def mainPage():
-    index = open('index.html')
-    return(index.read())
+    return render_template('index.html')
+
+@app.route("/api/EmpData",methods=['get'])
+def getData():
+    try:
+        Eid = request.args.get('Eid')
+    except:
+        data = {'Eid' : None}
+        return(json.dumps(data))
+    for i in volatile_db:
+        if(i.Eid == Eid):
+            data = {
+                'Eid' : i.Eid,
+                'Ename': i.Ename,
+                'Esal' : i.Esal,
+                'HRA' : i.HRA,
+                'DA' : i.DA,
+                'Ded' : i.Ded
+            }
+            return(json.dumps(data))
+    data = {'Eid' : None}
+    return(json.dumps(data))
+
+@app.route("/api/AddData",methods=['get'])
+def AddData():
+    try:
+        print(request.args)
+        Eid = request.args.get('Eid')
+        Ename = request.args.get('Ename')
+        Esal = request.args.get('Esal')
+        HRA = request.args.get('Ehra')
+        DA = request.args.get('Eda')
+        Ded = request.args.get('Eded')   
+        volatile_db.append(Employee(Eid,Ename,Esal,HRA,DA,Ded))
+        return(render_template('result.html',state='Sucess!'))
+    except:
+        return(render_template('result.html',state='Failed!'))
+
+@app.route("/api/ChangeData",methods=['get'])
+def ChangeData():
+    try:
+        Eid = request.args.get('Eid')
+        Ename = request.args.get('Ename')
+        Esal = request.args.get('Esal')
+        HRA = request.args.get('Ehra')
+        DA = request.args.get('Eda')
+        Ded = request.args.get('Eded')   
+        for i in range(len(volatile_db)):
+            if(volatile_db[i].Eid == Eid):
+                volatile_db[i]=Employee(Eid,Ename,Esal,HRA,DA,Ded)
+        print(volatile_db)
+        return(render_template('result.html',state='Sucess!'))
+    except Exception as ex:
+        return(str(ex))
+
 
 app.run(debug=True)
--- main.js
+++ main.js
@@ -5,4 +5,76 @@ function main()
     $("#save").attr("disabled",true)
     $("#change").attr("disabled",true)
     $("#reset").attr("disabled",true)
+
+    $('#Employee_id').change(CheckEid)
+
+    $('#change').click(UpdateValues)
+    $('#reset').click(resetvals)
+}
+
+function CheckEid()
+{   
+    if($('#Employee_id').val()=='')
+        return
+    requestData = {Eid : $('#Employee_id').val()}
+    params = $.param(requestData)
+    $.get('http://127.0.0.1:5000/api/EmpData?'+params).done(
+        function(data)
+        {
+            data = JSON.parse(data)
+            if(data.Eid == null)
+            {
+                $('#save').attr('disabled',false)
+                $('#reset').attr('disabled',false)
+                $('#change').attr('disabled',true)
+
+                $('#Employee_name').val('')
+                $('#Employee_salary').val(null)
+                $('#Employee_hra').val(null)
+                $('#Employee_da').val(null)
+                $('#Employee_deductions').val(null)
+            }
+            else
+            {
+                $('#save').attr('disabled',true)
+                $('#reset').attr('disabled',false)       
+                $('#change').attr('disabled',false)
+
+                $('#Employee_id').attr('disabled',true)
+                
+                $('#Employee_name').val(data.Ename)
+                $('#Employee_salary').val(data.Esal)
+                $('#Employee_hra').val(data.HRA)
+                $('#Employee_da').val(data.DA)
+                $('#Employee_deductions').val(data.Ded)
+            }
+        })  
+}
+
+function UpdateValues(event)
+{
+    event.preventDefault()
+    reqData = {
+        Eid : $('#Employee_id').val(),
+        Esal : $('#Employee_salary').val(),
+        Ename : $('#Employee_name').val(),
+        Ehra : $('#Employee_hra').val(),
+        Eda : $('#Employee_da').val(),
+        Eded : $('#Employee_deductions').val()
+    }
+    params = $.param(reqData)
+    location.href = ('http://127.0.0.1:5000/api/ChangeData?'+params);
+}
+
+function resetvals(event)
+{
+    event.preventDefault()
+    $('#Employee_id').attr('disabled',false)
+    $('#Employee_id').val('')
+    $('#Employee_name').val('')
+    $('#Employee_salary').val(null)
+    $('#Employee_hra').val(null)
+    $('#Employee_da').val(null)
+    $('#Employee_deductions').val(null)
+    $('#Employee_id').focus()
 }
